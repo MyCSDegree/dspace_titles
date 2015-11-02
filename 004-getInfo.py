77504b6553af6310afa81e5d5a25a0f84bf34bb3
thewisenerd <thewisenerd@protonmail.com>
2015-11-02 14:50:06 +0530

    add 004-getInfo.py

--- 004-getInfo.py
+++ 004-getInfo.py
@@ -1,33 +1,46 @@
+#!/usr/bin/env python
+
 import os
 from bs4 import BeautifulSoup
 import sys
 import re
 import json
+import glob
+from subprocess import call
 
-json_file = 'JSON/' + sys.argv[1] + '.json'
+import time
 
-if os.path.exists(os.path.dirname(json_file)):
-  quit()
+DSPACE_BASE="10.1.32.27/dspace/handle/123456789/"
 
-print "processing " + sys.argv[1] + '.json'
-soup = BeautifulSoup(open(sys.argv[1]), 'lxml')
-
-table = soup.find('table', {'class' : 'itemDisplayTable'});
-
-meta_s = table.find_all('tr')
-
-data = {}
-
-for meta in meta_s:
-  label = meta.find('td', {'class' : 'metadataFieldLabel'}).text.encode('utf-8')
-  value = meta.find('td', {'class' : 'metadataFieldValue'}).text.encode('utf-8')
-  data[re.sub('[^A-Za-z0-9]+', '', label)] = value
-
-data['urlhref'] = soup.find('td', {'class' : 'standard'}).find('a').get('href').encode('utf-8')
-
-
-if not os.path.exists(os.path.dirname(json_file)):
-  os.makedirs(os.path.dirname(json_file))
-
-with open(json_file, 'w') as fp:
-  json.dump(data, fp)
+for myfile in glob.iglob(DSPACE_BASE + "*"):
+  file_split = myfile.split("/")
+  filenam = file_split[-1:][0]
+  my_dir = 'DATA/' + '/'.join(file_split[-2:])
+  if os.path.exists(my_dir + '/' + filenam +'.json'):
+    continue
+  if not os.path.exists(os.path.dirname(my_dir)):
+    os.makedirs(os.path.dirname(my_dir))
+  json_file = my_dir + '/' + filenam + '.json'
+  print "processing " + json_file
+  soup = BeautifulSoup(open(myfile), 'lxml')
+  table = soup.find('table', {'class' : 'itemDisplayTable'});
+  meta_s = table.find_all('tr')
+  data = {}
+  for meta in meta_s:
+    label = meta.find('td', {'class' : 'metadataFieldLabel'}).text.encode('utf-8')
+    value = meta.find('td', {'class' : 'metadataFieldValue'}).text.encode('utf-8')
+    data[re.sub('[^A-Za-z0-9]+', '', label)] = value
+  
+  data['urlhref'] = []
+  for td in soup.find_all('td', {'class' : 'standard'}):
+    if (td.find('a') is not None):
+      url = td.find('a').get('href').encode('utf-8')
+      if url in data['urlhref']:
+        continue
+      data['urlhref'].append(url)
+      pdf = my_dir + url.split("/" + filenam,1)[1] 
+      os.system("/bin/bash -c 'curl --progress-bar http://10.1.32.27" + url + " --create-dirs -o " + pdf + "'")
+      os.system("/bin/bash -c 'touch " + json_file + "'")
+  with open(json_file, 'w') as fp:
+    json.dump(data, fp)
+  time.sleep(1)
